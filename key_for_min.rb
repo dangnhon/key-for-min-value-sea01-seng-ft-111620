602ab51855d02e2cd350b5cccb607266b7ed12e0
Nhon Dang <dang.nhon12345@yahoo.com>
2020-10-27 21:58:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-lowest = ""
+lowest = 
 low_price = 100
 
  name_hash.each do |name, price|
    if price < low_price
      low_price = price
-     lowest = name
+     lowest = name[price] 
    end
  end
  puts #{lowest}
